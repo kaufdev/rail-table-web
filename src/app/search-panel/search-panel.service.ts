1020464ea3681c6ca59f27f1069e8834a903e8d8
kaufdev <kaufdev@protonmail.com>
2021-06-04 02:18:11 +0200

    Integration with backend stationSearching

--- src/app/search-panel/search-panel.service.ts
+++ src/app/search-panel/search-panel.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -10,17 +10,6 @@ import { Transfer } from '../model/transfer.model';
     providedIn: 'root',
    })
 export class SearchPanelService{
-    stations: Station[] = [
-        new Station('Kraków Podgórze', 'Kraków', 'KRP'),
-        new Station('Kraków Główny', 'Kraków', 'KRK'),
-        new Station('Kraków Olsza', 'Kraków', 'KRO'),
-        new Station('Warszawa Centralna', 'Warszawa', 'WAC'),
-        new Station('Warszawa Główna', 'Warszawa', 'WAG'),
-        new Station('Warszawa Stadion', 'Warszawa', 'WAS'),
-        new Station('Gdańsk Główny', 'Gdańsk', 'GDS'),
-        new Station('Gdynia', 'Gdańsk', 'GDN')
-    ];
-
     constructor(private http: HttpClient){}
 
     searchTransfer(dto: SearchModelDto): Observable<Transfer[]> {
@@ -28,11 +17,8 @@ export class SearchPanelService{
     }
 
     searchStation(stationProposal: string): Observable<Station[]>{
-        const response: Station[] = this.stations.filter(station => {
-            const lowerCaseStationName: string = station.name.toLowerCase();
-            return lowerCaseStationName.startsWith(stationProposal.toLowerCase());
-        }
-            );
-        return new BehaviorSubject<Station[]>(response);
+        let params: HttpParams = new HttpParams();
+        params = params.append('stationNameProposal', stationProposal);
+        return this.http.get<Station[]>(environment.apiUrl + 'station/search', {params});
     }
 }
